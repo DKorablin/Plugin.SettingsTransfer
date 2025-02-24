cee60d359ba2bd5b30f7d2a3ed41d203494124f4
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:32:20 +0300

    Unify plugins deployment

--- Plugin.SettingsTransfer/Properties/AssemblyInfo.cs
+++ Plugin.SettingsTransfer/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("b169928a-b934-481e-8a11-f0a1b702149c")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.SettingsTransfer")]
 #else
 
-[assembly: AssemblyTitle("Plugin.SettingsTransfer")]
 [assembly: AssemblyDescription("Copy plugins settings from one data sorce to anoter")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.SettingsTransfer")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2017-2024")]
 #endif
